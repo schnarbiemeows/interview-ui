c5677de7a522fce8f96ecd8824fbf2969a861242
schnarbiemeows <dylanikessler@yahoo.com>
2021-06-14 17:50:41 -0400

    enhancement: added a collapse and a transition to the answer on the main page

--- src/app/components/mainpage/mainpage.component.ts
+++ src/app/components/mainpage/mainpage.component.ts
@@ -16,7 +16,8 @@ import {AnswerService} from "../../services/answer/answer.service";
 
 @Component({
   selector: 'app-mainpage',
-  templateUrl: './mainpage.component.html'
+  templateUrl: './mainpage.component.html',
+  styleUrls: ['./mainpage.component.css']
 })
 export class MainpageComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription[] = [];
